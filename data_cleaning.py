69bd20d4605d68ff8e29c28b64f06d6fa0eddc92
Gianmario Voria <voriagianmario@gmail.com>
2022-07-19 19:33:47 +0200

    defined queries and cleaning steps; page layout

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,1 +1,11 @@
 # qui ci mettiamo tutte le funzioni utili alla pulizia del dataset
+
+# drop na
+
+# drop colonne developer e publisher
+
+# drop nomi di gioco uguali
+
+# drop su recensioni senza info
+
+# lavorare su recensione per renderlo da frase a campo combinato {apprezzamento, numero_recensioni}
--- query_manager.py
+++ query_manager.py
@@ -1,1 +1,22 @@
 # qui ci mettiamo tutte le query 
+
+
+# inserimento nuovo gioco
+
+# modifica informazioni di un videogioco
+
+# cancellazione di un videogioco
+
+# ricerca per nome
+
+# ricerca per categoria (anche più di una)
+
+# tutte le possibili ricerche per la recensione (strutturato)
+
+# stampa top 20 per recensione in una data categoria
+
+# ricerca ordinata per data
+
+# ricerca ordinata per prezzo
+
+# consigliare 10 giochi con buon rapporto prezzo/recensione (ordiniamo per #recensioni/valore_recensione e per prezzo)
